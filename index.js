1662ef0040021c314479dc86327b190d5a9a0b8f
enisanila <nilasarienisa@gmail.com>
2023-12-06 00:48:21 +0700

    uploudfile

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dbConfig = require('./config/db.config');
-const auth = require('./middlewares/auth');
-const errors = require('./middlewares/errors');
-const { unless } = require('express-unless');
+const functions = require('firebase-functions');
+const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./config/db.config");
+
+const auth = require("./middlewares/auth");
+const errors = require("./middlewares/errors");
+
+const {unless} = require("express-unless");
 const app = express();
 
 mongoose.Promise = global.Promise;
-
-const connectToDB = async () => {
-  try {
-    await mongoose.connect(dbConfig.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected');
-  } catch (error) {
-    console.log('Database connection error:', error);
-  }
-};
-
-connectToDB();
+mongoose
+    .connect(dbConfig.MONGO_URI, {
+        useNewUrlParser: true,  
+        useUnifiedTopology: true, 
+    })
+    .then(
+        () => {
+            console.log("Database connected");
+        },
+        (error) => {
+            console.log("Database can't be connected: " + error);
+        }
+    );
 
 auth.authenticateToken.unless = unless;
 app.use(
-  auth.authenticateToken.unless({
-    path: [
-      { url: '/users/login', methods: ['POST'] },
-      { url: '/users/register', methods: ['POST'] },
+    auth.authenticateToken.unless({
+        path: [
+            { url: "/users/login", methods: ["POST"] },
+            { url: "/users/register", methods: ["POST"] },
     ],
-  })
+    })
 );
 
 app.use(express.json());
 
-app.use('/users', require('./routes/users.routes'));
+app.use("/users", require("./routes/users.routes"));
 
 app.use(errors.errorHandler);
-// Default Routes 
 app.get('/', (req, res) => {
-    res.json('message: Hello World!');
-    res.status(200);
+    res.send('Hello World!')
+  })
+
+app.listen(process.env.PORT || 4000, function () {
+    console.log("Ready to GO!");
 });
-exports.app = async (req, res) => {
-
-  if (mongoose.connection.readyState !== 1) {
-    await connectToDB();
-  }
-
-  app(req, res);
-};
+exports.app = functions.https.onRequest(app);
